35c60c9df9e8e90a628663d024c80206efd75508
Jingwei <zhjw43@163.com>
2019-06-02 13:44:46 +0800

    refactor example

--- example_test.go
+++ example_test.go
@@ -8,34 +8,44 @@ import (
 	"github.com/chalvern/gochan"
 )
 
+type Manager struct {
+	dispatcher *gochan.Dispatcher
+}
+
+func (m *Manager) Dispatch(objID int, task gochan.TaskFunc) {
+	m.dispatcher.Dispatch(objID, task)
+}
+
 func TestDispatcher(t *testing.T) {
 	gochanNum := 3
 	bufferNum := 10
-	dsp := gochan.NewDispatcher(gochanNum, bufferNum)
+	manager := Manager{
+		dispatcher: gochan.NewDispatcher(gochanNum, bufferNum),
+	}
 
 	objID := 1
 	task1 := func() error {
 		return errors.New("task 1")
 	}
-	dsp.Dispatch(objID, task1)
+	manager.Dispatch(objID, task1)
 
 	objID = 2
 	task2 := func() error {
 		return errors.New("task 2")
 	}
-	dsp.Dispatch(objID, task2)
+	manager.Dispatch(objID, task2)
 
 	objID = 3
 	task3 := func() error {
 		return errors.New("task 3")
 	}
-	dsp.Dispatch(objID, task3)
+	manager.Dispatch(objID, task3)
 
 	objID = 4
 	task4 := func() error {
 		return errors.New("task 4")
 	}
-	dsp.Dispatch(objID, task4)
+	manager.Dispatch(objID, task4)
 
 	time.Sleep(time.Second)
 }
