33f7b945099bd66fec5428187ccb9f941ee419b3
Jingwei <zhjw43@163.com>
2019-06-02 17:23:04 +0800

    add example packages

---
+++ examples/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"errors"
+	"math/rand"
+	"time"
+
+	"github.com/chalvern/gochan"
+)
+
+type Manager struct {
+	gochanNum  int
+	bufferNum  int
+	dispatcher *gochan.Dispatcher
+}
+
+func (m *Manager) Dispatch(objID int, task gochan.TaskFunc) error {
+	if objID < 0 {
+		objID = rand.Intn(m.gochanNum)
+	}
+	return m.dispatcher.Dispatch(objID, task)
+}
+
+func (m *Manager) Close() {
+	m.dispatcher.Close()
+}
+
+func main() {
+	gochanNum := 3
+	bufferNum := 10
+	manager := Manager{
+		gochanNum:  gochanNum,
+		bufferNum:  bufferNum,
+		dispatcher: gochan.NewDispatcher(gochanNum, bufferNum),
+	}
+
+	objID := 1
+	task1 := func() error {
+		return errors.New("task 1")
+	}
+	manager.Dispatch(objID, task1)
+	time.Sleep(time.Second)
+}
